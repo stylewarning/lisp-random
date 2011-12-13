c2f9371b366fccfedcd410887872fa1ddb8f07f9
Robert Smith <quad@symbo1ics.com>
2011-12-13 04:49:45 -0700

    simple inefficient FFT

--- fft.lisp
+++ fft.lisp
@@ -7,10 +7,14 @@
       1
       (exp (/ (* 2*pi*i k) n))))
 
-(defun dft (x &key
-                (normalization-factor (/ (sqrt (length x))))
-                inversep
-                )
+(defun slice (v indexes)
+  (loop
+    :for i :in indexes
+    :collect (aref v i) :into s
+    :finally (return (coerce s 'vector))))
+
+(defun dft (x &key (normalization-factor (/ (sqrt (length x))))
+                   inversep)
   (let ((N (length x)))
     (labels ((coeff (k)
                (loop
@@ -21,4 +25,20 @@
       (loop :for k :below N
             :collect (* (coeff k) normalization-factor) :into dft-array
             :finally (return (coerce dft-array 'vector))))))
+
+;;; This has a normalization factor of 1
+(defun fft (x)
+  (let ((N (length x)))
+    (if (= 1 N)
+        x
+        (let* ((m (floor n 2))
+               (top (fft (slice x (qtl:range 0 N 2))))
+               (bot (fft (slice x (qtl:range 1 N 2))))
+               (d (map 'vector
+                       (lambda (k) (primitive-nth-root N (- k)))
+                       (qtl:iota m)))
+               (z (map 'vector #'* d bot)))
+          (concatenate 'vector
+                       (map 'vector (lambda (vi zi) (+ vi zi)) top z)
+                       (map 'vector (lambda (vi zi) (- vi zi)) top z))))))
 
