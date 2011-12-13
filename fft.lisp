7724510e3c644ebaac7b2a6e51343e4f6a6dfbd7
Robert Smith <quad@symbo1ics.com>
2011-12-13 05:13:31 -0700

    got rid of macros in fft

--- fft.lisp
+++ fft.lisp
@@ -13,24 +13,20 @@
     :collect (aref v i) :into s
     :finally (return (coerce s 'vector))))
 
-
-(defmacro mapvec (f &rest vecs)
-  `(map 'vector ,f ,@vecs))
-
-(defmacro catvec (&rest vecs)
-  `(concatenate 'vector ,@vecs))
+(defun catvec (v1 v2)
+  (concatenate 'vector v1 v2))
 
 (defun .+ (v1 v2)
-  (mapvec #'+ v1 v2))
+  (map 'vector #'+ v1 v2))
 
 (defun .- (v1 v2)
-  (mapvec #'+ v1 v2))
+  (map 'vector #'+ v1 v2))
 
 (defun .* (v1 v2)
-  (mapvec #'* v1 v2))
+  (map 'vector #'* v1 v2))
 
 (defun .^ (v1 v2)
-  (mapvec #'expt v1 v2))
+  (map 'vector #'expt v1 v2))
 
 
 (defun dft (x &key (normalization-factor (/ (sqrt (length x))))
