a7639fcf8f066c1011f642beb832c763a8726ded
Robert Smith <quad@symbo1ics.com>
2012-01-21 16:32:23 -0700

    some minor cleanup to the simple fft

--- fft.lisp
+++ fft.lisp
@@ -1,19 +1,15 @@
+;;;; Copyright (c) 2012 Robert Smith
+;;;; Simple FFT
+;;;; Requires QTILITY
+
+;;; Constants
+
 (defconstant ii #C(0 1))
+
 (defconstant 2*pi*i (* 2 pi ii))
 
-(defun primitive-nth-root (n &optional (k 1))
-  (if (= n k)
-      1
-      (exp (/ (* 2*pi*i k) n))))
 
-(defun slice (v indexes)
-  (loop
-    :for i :in indexes
-    :collect (aref v i) :into s
-    :finally (return (coerce s 'vector))))
-
-(defun catvec (v1 v2)
-  (concatenate 'vector v1 v2))
+;;; Utilities
 
 (defmacro define-pointwise-operation (name function)
   (let ((v1 (gensym "V1-"))
@@ -27,6 +23,35 @@
 (define-pointwise-operation ./ #'/)
 (define-pointwise-operation .^ #'expt)
 
+(defun slice (v indexes)
+  (loop
+    :for i :in indexes
+    :collect (aref v i) :into s
+    :finally (return (coerce s 'vector))))
+
+(defun catvec (v1 v2)
+  (concatenate 'vector v1 v2))
+
+(defun primitive-nth-root (n &optional (k 1))
+  (if (= n k)
+      1
+      (exp (/ (* 2*pi*i k) n))))
+
+;;; FFT
+(defun fft (x)
+  ;; This has a normalization factor of 1
+  (let ((N (length x)))
+    (if (= 1 N)
+        x
+        (let* ((top   (fft (slice x (qtl:range 0 N 2))))
+               (bot   (fft (slice x (qtl:range 1 N 2))))
+               (roots (qtl:tabulate (lambda (k) (primitive-nth-root N (- k)))
+                                    (floor N 2)))
+               (z     (.* roots bot)))
+          (catvec (.+ top z) (.- top z))))))
+
+
+;;; DFT
 (defun dft (x &key (normalization-factor (/ (sqrt (length x))))
                    inversep)
   (let ((N (length x)))
@@ -39,16 +64,3 @@
       (loop :for k :below N
             :collect (* (coeff k) normalization-factor) :into dft-array
             :finally (return (coerce dft-array 'vector))))))
-
-;;; This has a normalization factor of 1
-(defun fft (x)
-  (let ((N (length x)))
-    (if (= 1 N)
-        x
-        (let* ((top   (fft (slice x (qtl:range 0 N 2))))
-               (bot   (fft (slice x (qtl:range 1 N 2))))
-               (roots (qtl:tabulate (lambda (k) (primitive-nth-root N (- k)))
-                                    (floor N 2)))
-               (z     (.* roots bot)))
-          (catvec (.+ top z) (.- top z))))))
-
