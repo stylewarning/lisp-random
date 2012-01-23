e5baf7c6d38765357f81a162d3647faa3d764c14
Robert Smith <quad@symbo1ics.com>
2012-01-22 18:28:01 -0700

    added declarations to kasadkad's problem

--- kasadkad.lisp
+++ kasadkad.lisp
@@ -1,7 +1,11 @@
 ;;;; Kasadkad's avoiding stuff
 ;;;; Copyright (c) 2012 Robert Smith
 
+(declaim (optimize speed (safety 0) (debug 0)))
+
 ;;; UTILITY FUNCTIONS
+
+(declaim (inline xor list-to-vector range iota))
 
 (defun xor (a b)
   "Exclusive or on A and B."
@@ -10,24 +14,34 @@
 
 (defun list-to-vector (list)
   "Convert LIST into a vector."
+  (declare (type list list))
+  
   (make-array (length list) :initial-contents list))
 
 (defun range (start end &optional (step 1))
   "Return the list of numbers n such that START <= n < END and
 n = START + k*STEP."
+  (declare (type fixnum start end step))
   (assert (<= start end))
+
   (loop :for i :from start :below end :by step :collecting i))
 
 (defun iota (n)
   "Return [0, ..., N-1]."
-  (assert (non-negative-p n))
+  (declare (type fixnum n))
+  (assert (not (minusp n)))
+
   (range 0 n))
 
 ;;; PERMUTATIONS
 
 (defun order-isomorphic-p (a b)
   "Are A and B order-isomorphic?"
+  (declare (type vector a b))
+  
   (let ((n (length a)))
+    (declare (type fixnum n))
+    
     (when (= n (length b))
       (loop :for i :below n
             :always (loop :for j :from i :below n
@@ -35,7 +49,12 @@ n = START + k*STEP."
                                             (<= (elt b i) (elt b j)))))))))
 (defun subsequences (x n)
   "Get all of the subsequences of X of length N."
+  (declare (type fixnum n)
+           (type vector x))
+  
   (let ((lenx (length x)))
+    (declare (type fixnum lenx))
+    
     (cond
       ((or (> n lenx)
            (not (plusp n))) nil)
@@ -47,17 +66,25 @@ n = START + k*STEP."
 (defun permutation-matches-p (perm pattern)
   "Does the permutation PERM have a subsequence which matches the
 pattern PATTERN?"
+  (declare (type vector perm pattern))
+  
   (loop :for s :in (subsequences perm (length pattern))
         :thereis (order-isomorphic-p pattern s)))
 
 (defun permutation-avoids-p (perm pattern)
   "Does the permutation PERM avoid the pattern PATTERN?"
+  (declare (type vector perm pattern))
+  
   (not (permutation-matches-p perm pattern)))
 
 ;;; This conses an awful lot.
 (defun permutations (n)
   "Generate the elements of the permutation group S_N."
+  (declare (type integer n))
+  
   (labels ((perms (l)
+             (declare (type list l))
+
              (if (null l)
                  (list nil)
                  (mapcan #'(lambda (x)
@@ -69,6 +96,7 @@ pattern PATTERN?"
   "Compute a list of all of the patterns of size PATTERN-SIZE that
   avoid the permutation PERMUTATION."
   (declare (type (unsigned-byte 16) pattern-size))
+  
   (let ((perms (permutations pattern-size)))
     (delete-if (lambda (pattern)
                  (permutation-matches-p permutation pattern))
