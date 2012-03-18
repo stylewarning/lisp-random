9832c38c59d6c6089fbd3537805ba578963278d5
Robert Smith <quad@symbo1ics.com>
2012-03-17 21:23:54 -0600

    some interval arithmetic stuff

---
+++ interval.lisp
@@ -0,0 +1,62 @@
+;;;; interval arithmetic
+;;;; Copyright (c) 2012 Robert Smith
+
+(defstruct (interval (:conc-name)
+                     (:constructor interval (left right)))
+  left
+  right)
+
+(defmacro with-iv (iv (a b) &body body)
+  "Destructuring mechanism for intervals."
+  (let ((iv-once (gensym)))
+    `(let ((,iv-once ,iv))
+       (let ((,a (left ,iv-once))
+             (,b (right ,iv-once)))
+         ,@body))))
+
+(defun zero-in (iv)
+  "Is zero in IV?"
+  (<= (left iv) 0 (right iv)))
+
+(defun iv+ (x y)
+  (with-iv x (a b)
+    (with-iv y (c d)
+      (let ((a+c (+ a c))
+            (a+d (+ a d))
+            (b+c (+ b c))
+            (b+d (+ b d)))
+        (interval (min a+c a+d b+c b+d)
+                  (max a+c a+d b+c b+d))))))
+
+(defun iv- (x y)
+  (with-iv x (a b)
+    (with-iv y (c d)
+      (let ((a-c (- a c))
+            (a-d (- a d))
+            (b-c (- b c))
+            (b-d (- b d)))
+        (interval (min a-c a-d b-c b-d)
+                  (max a-c a-d b-c b-d))))))
+
+(defun iv* (x y)
+  (with-iv x (a b)
+    (with-iv y (c d)
+      (let ((a*c (* a c))
+            (a*d (* a d))
+            (b*c (* b c))
+            (b*d (* b d)))
+        (interval (min a*c a*d b*c b*d)
+                  (max a*c a*d b*c b*d))))))
+
+(defun iv/ (x y)
+  (if (zero-in y)
+      (error (make-condition 'division-by-zero :operation 'iv/
+                                               :operands (list x y)))
+      (with-iv x (a b)
+        (with-iv y (c d)
+          (let ((a/c (/ a c))
+                (a/d (/ a d))
+                (b/c (/ b c))
+                (b/d (/ b d)))
+            (interval (min a/c a/d b/c b/d)
+                      (max a/c a/d b/c b/d)))))))
