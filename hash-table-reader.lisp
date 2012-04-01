2fd9cbb94254195944e39c32f68a13209f2b64ff
Robert Smith <quad@symbo1ics.com>
2012-03-31 21:42:45 -0600

    hash-table-reader

---
+++ hash-table-reader.lisp
@@ -0,0 +1,21 @@
+(defun alist-to-hash-table (alist)
+  (loop :with ht := (make-hash-table)
+        :for (k v) :in alist
+        :do (setf (gethash k ht) (eval v))
+        :finally (return ht)))
+
+(set-macro-character
+ #\{
+ (lambda (stream char)
+   (declare (ignore char))
+   (let ((lst (read-delimited-list #\} stream t)))
+     (alist-to-hash-table lst))))
+
+
+;;; CL-USER> {(:x 1) (:y 2) (:z (+ 1 1))}
+;;; #<HASH-TABLE :TEST EQL :COUNT 3 {128A8141}>
+;;; CL-USER> '{(:x 1) (:y 2) (:z (+ 1 1))}
+;;; #<HASH-TABLE :TEST EQL :COUNT 3 {128B0641}>
+;;; CL-USER> (gethash :z {(:x 1) (:y 2) (:z (+ 1 1))})
+;;; 2
+;;; T
