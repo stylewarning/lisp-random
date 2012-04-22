b31d0f9c4d85f0cc8e07ce9a586f5d3ec6c4e33d
Robert Smith <quad@symbo1ics.com>
2012-04-22 03:52:28 -0600

    change some variable names. Add BUILD-NAIVE-CIRCUIT.

--- m-of-n-circuit.lisp
+++ m-of-n-circuit.lisp
@@ -11,46 +11,65 @@
   (and a b c))
 
 ;;; We could optimalize to check that there are >= M inputs.
-(defun any-m-of-n-slow (m inputs)
-  "Check that any M of the INPUTS is T, eagerly evaluating the
+(defun any-m-of-n-slow (m &rest x)
+  "Check that any M of the inputs X are T, eagerly evaluating the
 inputs."
-  (cond
-    ((zerop m) t)
-    ((null inputs) nil)
-    ((car inputs) (any-m-of-n (1- m) (cdr inputs)))
-    (t (any-m-of-n m (cdr inputs)))))
+  (check-type m integer)
+  
+  (labels ((f (m x)
+             (cond
+               ((zerop m) t)
+               ((null x)  nil)
+               ((first x) (f (1- m) (rest x)))
+               (t         (f m (rest x))))))
+    (f m x)))
 
 ;;; Here we keep track of the number of inputs so we can fail quickly.
-(defun any-m-of-n (m &rest inputs)
-    "Check that any M of the INPUTS is T, eagerly evaluating the
+(defun any-m-of-n (m &rest x)
+    "Check that any M of the inputs X are T, eagerly evaluating the
 inputs. Fail as soon as we reach fewer inputs than M."
   (check-type m integer)
 
-  (labels ((any (m len inputs)
+  ;; Here, N keeps track of the length of X.
+  (labels ((f (m n x)
              (cond
                ((not (plusp m)) t)
-               ((> m len) nil)
-               ((car inputs) (any (1- m) (1- len) (cdr inputs)))
-               (t (any m (1- len) (cdr inputs))))))
-    (any m (length inputs) inputs)))
+               ((> m n)         nil)
+               ((first x)       (f (1- m) (1- n) (rest x)))
+               (t               (f m (1- n) (rest x))))))
+    (f m (length x) x)))
 
-(defun build-circuit (m inputs)
-  "Build a boolean circuit which checks if M of the INPUTS are T."
+(defun build-naive-circuit (m x)
+  "Build a boolean circuit which checks if M of the inputs X are T."
   (check-type m integer)
-  (check-type inputs list)
+  (check-type x list)
   
-  (labels ((any (m len inputs)
+  (labels ((f (m n x)
              (cond
-               ((and (= 1 m) (< 1 len)) `(or ,@inputs))
-               ((= 1 m len)  (car inputs))
-               ((= m len)    `(and ,@inputs))
-               (t            `(if ,(car inputs)
-                                  ,(any (1- m) (1- len) (cdr inputs))
-                                  ,(any m (1- len) (cdr inputs)))))))
-    (let ((len (length inputs)))
+               ((not (plusp m)) t)
+               ((> m n)         nil)
+               (t               `(if ,(first x)
+                                     ,(f (1- m) (1- n) (rest x))
+                                     ,(f m (1- n) (rest x)))))))
+    (f m (length x) x)))
+
+(defun build-circuit (m x)
+  "Build a boolean circuit which checks if M of the inputs X are T."
+  (check-type m integer)
+  (check-type x list)
+  
+  (labels ((f (m n x)
+             (cond
+               ((and (= 1 m) (< 1 n)) `(or ,@x))
+               ((= 1 m n)             (first x))
+               ((= m n)               `(and ,@x))
+               (t                     `(if ,(first x)
+                                           ,(f (1- m) (1- n) (rest x))
+                                           ,(f m (1- n) (rest x)))))))
+    (let ((n (length x)))
       (cond ((not (plusp m)) t)
-            ((> m len) nil)
-            (t         (any m len inputs))))))
+            ((> m n)         nil)
+            (t               (f m n x))))))
 
 ;; CL-USER> (build-circuit 0 '(a b c d e))
 ;; T
@@ -103,17 +122,17 @@ inputs. Fail as soon as we reach fewer inputs than M."
   (values (remove-if (lambda (x) (true-like x environment)) things)
           (count-if (lambda (x) (true-like x environment)) things)))
 
-(define-compiler-macro any-m-of-n (&whole form m &rest inputs &environment env)
+(define-compiler-macro any-m-of-n (&whole form m &rest x &environment env)
   ;; First, delete any known null inputs. They decrease the circuit
   ;; size. This optimization might also get done by the compiler after
   ;; a circuit is computed.
   ;; 
   ;; Additionally, we remove any values which are "known truths", and
   ;; record the number of them, so we can decrease M.
-  (multiple-value-bind (scrubbed-inputs truths)
-      (remove-true-like-forms (remove-if #'null inputs) env)
-    (cond ((integerp m) (build-circuit (- m truths) scrubbed-inputs))
-          ((plusp truths) `(any-m-of-n (- ,m ,truths) ,@scrubbed-inputs))
+  (multiple-value-bind (scrubbed-x truths)
+      (remove-true-like-forms (remove-if #'null x) env)
+    (cond ((integerp m) (build-circuit (- m truths) scrubbed-x))
+          ((plusp truths) `(any-m-of-n (- ,m ,truths) ,@scrubbed-x))
           (t form))))
 
 ;; CL-USER> (funcall (compiler-macro-function 'any-m-of-n)
