fcbc9d7ef2ad832eccef7e9653e6c909fe72f1ff
Robert Smith <quad@symbo1ics.com>
2012-04-22 00:12:23 -0600

    clean up BUILD-CIRCUIT, and add example

--- m-of-n-circuit.lisp
+++ m-of-n-circuit.lisp
@@ -12,6 +12,8 @@
 
 ;;; We could optimalize to check that there are >= M inputs.
 (defun any-m-of-n (m inputs)
+  "Check that any M of the INPUTS is T, eagerly evaluating the
+inputs."
   (cond
     ((zerop m) t)
     ((null inputs) nil)
@@ -20,6 +22,8 @@
 
 ;;; Here we keep track of the number of inputs so we can fail quickly.
 (defun any-m-of-n-quickfail (m inputs)
+    "Check that any M of the INPUTS is T, eagerly evaluating the
+inputs. Fail as soon as we reach fewer inputs than M."
   (labels ((any (m len inputs)
              (cond
                ((zerop m) t)
@@ -29,15 +33,62 @@
     (any m (length inputs) inputs)))
 
 (defun build-circuit (m inputs)
+  "Build a boolean circuit which checks if M of the INPUTS are T."
+  (check-type m (integer 0))
+  (check-type inputs list)
+  
   (labels ((any (m len inputs)
              (cond
-               ((zerop m) t)
-               ((> m len) nil)
-               ((and (= 1 m)
-                     (< 1 len)) `(or ,@inputs))
-               ((= 1 m len) (car inputs))
-               ((= m len) `(and ,@inputs))
-               (t `(if ,(car inputs)
-                       ,(any (1- m) (1- len) (cdr inputs))
-                       ,(any m (1- len) (cdr inputs)))))))
-    (any m (length inputs) inputs)))
+               ((and (= 1 m) (< 1 len)) `(or ,@inputs))
+               ((= 1 m len)  (car inputs))
+               ((= m len)    `(and ,@inputs))
+               (t            `(if ,(car inputs)
+                                  ,(any (1- m) (1- len) (cdr inputs))
+                                  ,(any m (1- len) (cdr inputs)))))))
+    (let ((len (length inputs)))
+      (cond ((zerop m) t)
+            ((> m len) nil)
+            (t         (any m len inputs))))))
+
+;; CL-USER> (build-circuit 0 '(a b c d e))
+;; T
+;;
+;; CL-USER> (build-circuit 1 '(a b c d e))
+;; (OR A B C D E)
+;;
+;; CL-USER> (build-circuit 2 '(a b c d e))
+;; (IF A
+;;     (OR B C D E)
+;;     (IF B
+;;         (OR C D E)
+;;         (IF C
+;;             (OR D E)
+;;             (AND D E))))
+;;
+;; CL-USER> (build-circuit 3 '(a b c d e))
+;; (IF A
+;;     (IF B
+;;         (OR C D E)
+;;         (IF C
+;;             (OR D E)
+;;             (AND D E)))
+;;     (IF B
+;;         (IF C
+;;             (OR D E)
+;;             (AND D E))
+;;         (AND C D E)))
+;;
+;; CL-USER> (build-circuit 4 '(a b c d e))
+;; (IF A
+;;     (IF B
+;;         (IF C
+;;             (OR D E)
+;;             (AND D E))
+;;         (AND C D E))
+;;     (AND B C D E))
+;;
+;; CL-USER> (build-circuit 5 '(a b c d e))
+;; (AND A B C D E)
+;;
+;; CL-USER> (build-circuit 6 '(a b c d e))
+;; NIL
