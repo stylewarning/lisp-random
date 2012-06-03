2280873c649f5d46398cb528f33be68891dd15cd
Robert Smith <quad@symbo1ics.com>
2012-06-03 15:05:35 -0500

    add random DAG generation for testing

--- tsort.lisp
+++ tsort.lisp
@@ -49,3 +49,38 @@ directed acyclic graph."
                                ;; sinks. It must be cyclic!
                                (error "Cannot sort a cyclic graph. ~
                                        The cycles are ~S." dag))))))
+
+
+;;; Much of the functions in LABELS are taken from QTILITY.
+(defun random-dag (size)
+  (labels ((random-between (a b)
+             "Generate a random integer between A and B, inclusive."
+             (assert (>= b a))
+             (if (= a b)
+                 a
+                 (+ a (random (- (1+ b) a)))))
+           
+           (nshuffle (seq)
+             "Destructively shuffle SEQ."
+             (let ((n (length seq)))
+               (loop :for i :below n
+                     :for r := (random-between i (1- n))
+                     :when (/= i r)
+                       :do (rotatef (elt seq i)
+                                    (elt seq r))
+                     :finally (return seq))))
+           
+           (random-bool ()
+             "Generate a random boolean."
+             (zerop (random 2)))
+           
+           (random-deps (n)
+             "Generate a random list of dependencies up to SIZE."
+             (loop :for i :below n
+                   :when (random-bool)
+                     :collect i :into deps
+                   :finally (return deps))))
+    
+    (loop :for n :below size
+          :collect (cons n (nshuffle (random-deps n))) :into nodes
+          :finally (return (nshuffle nodes)))))
