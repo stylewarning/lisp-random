e01051a09fa689ad587c074d06a85ed99045abba
Robert Smith <quad@symbo1ics.com>
2012-08-02 09:32:53 -0500

    Add instruction datatype ISN, add ISN pretty printing

--- lisp86_64.lisp
+++ lisp86_64.lisp
@@ -29,16 +29,56 @@
 ;;;    rsp+8n = look at stack[n]
 ;;;    rsp-8n = allocate n words
 
+;;;;;;;;;;;;;;;;;;;; Instructions and Formatting ;;;;;;;;;;;;;;;;;;;;;
+
+(defstruct (isn (:conc-name isn.)
+                (:constructor make-isn (op &rest args)))
+  (op   :nop :type keyword)
+  (args nil  :type list))
+
+(defun format-isn (isn)
+  (with-output-to-string (s)
+    (labels ((kw (k)
+               (string-downcase (symbol-name k)))
+             (format-arg (arg)
+               (princ " " s)
+               (etypecase arg
+                 (integer (princ arg s))
+                 (keyword (princ (kw arg) s))
+                 (list (error "unimplemented formatting for ~S" arg)))))
+      
+      ;; Operator
+      (princ (kw (isn.op isn)) s)
+      
+      ;; Operands
+      (let ((args (isn.args isn)))
+        (case (length args)
+          ((0) nil)
+          ((1) (format-arg (first args)))
+          (otherwise
+           (progn
+             (dolist (arg (butlast args))
+               (format-arg arg)
+               (princ "," s))
+             (format-arg (first (last args))))))))))
+
+(defun print-isns (isn-list &optional (stream *standard-output*))
+  (dolist (isn isn-list)
+    (princ "        " stream)
+    (princ (format-isn isn) stream)
+    (terpri stream)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;; ASM generation ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun generate-asm (expr)
   (let ((isns nil))
-    (labels ((isn (&rest strs)
-               (push (apply 'concatenate 'string
-                            (mapcar 'princ-to-string strs))
+    (labels ((isn (op &rest args)
+               (push (apply 'make-isn op args)
                      isns))
              
              (gen (expr)
                (cond
-                 ((integerp expr) (isn "push " expr))
+                 ((integerp expr) (isn :push expr))
                  ((atom expr) (error "unknown thing ~S" expr))
                  ((= 3 (length expr))
                   (progn
@@ -49,15 +89,15 @@
                     (gen (third expr))
                     
                     ;; Pop arguments off the stack into registers.
-                    (isn "pop edx")
-                    (isn "pop eax")
+                    (isn :pop :edx)
+                    (isn :pop :eax)
                     
                     ;; Generate arithmetic.
                     (case (car expr)
                       ;; +-* all are easy.
-                      ((+) (isn "add eax, edx"))
-                      ((-) (isn "sub eax, edx"))
-                      ((*) (isn "imul eax, edx"))
+                      ((+) (isn :add :eax :edx))
+                      ((-) (isn :sub :eax :edx))
+                      ((*) (isn :imul :eax :edx))
                       
                       ;; division requires
                       ;;   - dividend is the concatenation
@@ -71,27 +111,27 @@
                       ;; in edx.
                       ((/ %)
                        ;; Get our divisor in place.
-                       (isn "mov ecx, edx")
+                       (isn :mov :ecx :edx)
                        
                        ;; Fill edx with appropriate sign bits.
-                       (isn "cdq")
+                       (isn :cdq)
                        
                        ;; Perform division.
-                       (isn "idiv ecx")
+                       (isn :idiv :ecx)
                        
                        ;; Move remainder into eax if we wanted that.
                        (when (eq (car expr) '%)
-                         (isn "mov eax, edx"))))
+                         (isn :mov :eax :edx))))
                     
                     ;; Push the result back onto the stack.
-                    (isn "push eax")))
+                    (isn :push :eax)))
                  (t (error "unknown thing ~S" expr)))))
       
       ;; Generate all the code.
       (gen expr)
       
       ;; Get result into eax.
-      (isn "pop eax")
+      (isn :pop :eax)
       
       ;; Return instructions.
       (nreverse isns))))
