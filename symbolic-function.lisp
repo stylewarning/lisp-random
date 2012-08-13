a90a7a3fe686296779719c479277052b6d51982b
Robert Smith <quad@symbo1ics.com>
2012-08-13 00:16:41 -0500

    Symbolic functions idea

---
+++ symbolic-function.lisp
@@ -0,0 +1,70 @@
+;;;; symbolic-function.lisp
+;;;; Copyright (c) 2012 Robert Smith
+
+;;; Requires MOP.
+#-closer-mop
+(ql:quickload "CLOSER-MOP")
+
+;;; A `symbolic function' is a function that holds both an executable
+;;; form of itself and a symbolic form of itself.
+
+(defclass symbolic-function ()
+  ((compiled-function :initarg :compiled-function
+                      :initform nil
+                      :accessor symbolic-function.compiled-function)
+
+   (parameters :initarg :parameters
+               :initform nil
+               :accessor symbolic-function.parameters)
+
+   (expression :initarg :expression
+               :initform nil
+               :accessor symbolic-function.expression))
+  (:metaclass c2mop:funcallable-standard-class))
+
+(defconstant +function-arrow+ #\↦
+  "The arrow printed in the printed representation of a
+  SYMBOLIC-FUNCTION.")
+
+(defmethod print-object ((sf symbolic-function) stream)
+  (print-unreadable-object (sf stream)
+    (with-slots (parameters expression) sf
+      (format
+       stream
+       "~S ~C ~S"
+       (if (endp (rest parameters))
+           (first parameters)
+           parameters)
+       +function-arrow+
+       expression))))
+
+(defun symbolic-function-lambda-form (sf)
+  "Create the lambda form (data) of SF."
+  `(lambda ,(symbolic-function.parameters sf)
+     ,(symbolic-function.expression sf)))
+
+(defun make-symbolic-function (parameters expression)
+  "Make a SYMBOLIC-FUNCTION with parameters PARAMETERS and expression
+  EXPRESSION."
+  (make-instance 'symbolic-function :parameters parameters
+                                    :expression expression))
+
+(defmethod initialize-instance :after ((sf symbolic-function) &key)
+  (with-slots (compiled-function parameters expression) sf
+    ;; Assert PARAMETERS is a list of symbols.
+    
+    ;; Grab the symbolic function or compute it, and set the
+    ;; FUNCALLABLE-INSTANCE-FUNCTION.
+    (let ((lam (or (symbolic-function.compiled-function sf)
+                   (setf (symbolic-function.compiled-function sf)
+                         (compile nil (symbolic-function-lambda-form sf))))))
+      (c2mop:set-funcallable-instance-function sf lam))))
+
+(defmacro define-symbolic-function (name param-list expression)
+  "Define a symbolic function NAME with the parameters PARAM-LIST,
+bound to the expression EXPRESSION."
+  `(progn
+     (setf (symbol-function ',name)
+           (make-symbolic-function ',param-list
+                                   ',expression))
+     ',name))
