40064779ce4725230d45cd9bdae48ee4e43e40cb
Robert Smith <quad@symbo1ics.com>
2012-08-29 00:12:36 -0500

    added lexical solution

--- macroexpand-count.lisp
+++ macroexpand-count.lisp
@@ -33,6 +33,7 @@ of expansions."
            (values (progn ,@body)
                    ,count))))))
 
+;;; Dynamic variables described by Bike.
 (defmacro count-macro-expansions-2 (&body body)
   "Count the number of macro expansions required to execute
 BODY. Return two values: the result of executing BODY and the number
@@ -46,3 +47,20 @@ of expansions."
                                                 (funcall expander form env)))))
          (values (progn ,@body)
                  ,count)))))
+
+
+;;; Lexical mish-mash described by pjb.
+(defmacro count-macro-expansions-3 (&body body)
+  "Count the number of macro expansions required to execute
+BODY. Return two values: the result of executing BODY and the number
+of expansions."
+  (let ((closures (gensym "CLOSURES-")))
+    `(let* ((,closures (funcall (compile nil `(lambda ()
+                                                (let ((count 0))
+                                                  (cons (lambda (expander form env)
+                                                          (incf count)
+                                                          (funcall expander form env)) 
+                                                        (lambda () count)))))))
+            (*macroexpand-hook* (car ,closures)))
+       (values (progn ,@body)
+               (funcall (cdr ,closures))))))
