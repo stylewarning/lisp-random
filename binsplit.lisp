ddd59e39a4756191bd54a41f0f9f82486dcdce4b
Robert Smith <quad@symbo1ics.com>
2012-09-01 04:31:18 -0500

    Add type declarations everywhere

--- binsplit.lisp
+++ binsplit.lisp
@@ -1,29 +1,45 @@
 ;;;; binsplit.pdf
 ;;;; Copyright (c) 2012 Robert Smith
 
+(declaim (optimize speed (safety 0) (debug 0)))
+
 (defun print-unreadable (object stream depth)
   (declare (ignore depth))
   (print-unreadable-object (object stream :type t :identity t)))
 
-(defun square (x) (* x x))
+(declaim (inline square cube))
 
-(defun cube (x) (* x x x))
+(defun square (x)
+  (declare (type integer x))
+  (* x x))
+
+(defun cube (x)
+  (declare (type integer x))
+  (* x x x))
+
+(deftype Z->Z () '(function (integer) integer))
 
 (defstruct (binsplit-series (:conc-name series.)
                             (:print-function print-unreadable))
-  a b p q)
+  (a (constantly 1) :type Z->Z :read-only t)
+  (b (constantly 1) :type Z->Z :read-only t)
+  (p (constantly 1) :type Z->Z :read-only t)
+  (q (constantly 1) :type Z->Z :read-only t))
 
 (defstruct (binsplit-computation (:conc-name comp.))
-  (p 0 :type integer)
-  (q 0 :type integer)
-  (b 0 :type integer)
-  (r 0 :type integer))
+  (p 0 :type integer :read-only t)
+  (q 0 :type integer :read-only t)
+  (b 0 :type integer :read-only t)
+  (r 0 :type integer :read-only t))
 
 (defun product (f lower upper)
   "Compute the product
  
   F(LOWER) * F(LOWER + 1) * ... * F(UPPER - 1)."
+  (declare (type integer lower upper)
+           (type (function (integer) integer) f))
   (labels ((rec (current accum)
+             (declare (type integer current accum))
              (if (>= current upper)
                  accum
                  (rec (1+ current)
@@ -43,7 +59,9 @@
                      (product q lower n))))))
 
 (defun binary-split-base-case=1 (series lower upper)
-  (declare (ignore upper))
+  (declare (ignore upper)
+           (type binsplit-series series)
+           (type integer lower upper))
   (make-binsplit-computation :p (funcall (series.p series) lower)
                              :q (funcall (series.q series) lower)
                              :b (funcall (series.b series) lower)
@@ -51,6 +69,8 @@
                                    (funcall (series.p series) lower))))
 
 (defun binary-split-base-case=n (series lower upper)
+  (declare (type binsplit-series series)
+           (type integer lower upper))
   (let ((b (product (series.b series) lower upper))
         (q (product (series.q series) lower upper)))
     (make-binsplit-computation :p (product (series.p series) lower upper)
@@ -59,6 +79,7 @@
                                :r (* b q (sum-series-direct series lower upper)))))
 
 (defun combine-computations (comp-left comp-right)
+  (declare (type binsplit-computation comp-left comp-right))
   (with-accessors ((pl comp.p)
                    (ql comp.q)
                    (bl comp.b)
@@ -74,14 +95,16 @@
                                        (* bl pl rr))))))
 
 (defun binary-split (series lower upper)
+  (declare (type binsplit-series series)
+           (type integer lower upper))
   (let ((delta (- upper lower)))
     (cond
       ((zerop delta) (error "UPPER must be greater than LOWER."))
       ((= 1 delta) (binary-split-base-case=1 series lower upper))
       #+#:bug
       ((< delta 5) (binary-split-base-case=n series lower upper))
-      (t (let* ((m    (floor (+ lower upper) 2))
-                (left (binary-split series lower m))
+      (t (let* ((m     (floor (+ lower upper) 2))
+                (left  (binary-split series lower m))
                 (right (binary-split series m upper)))
            (combine-computations left right))))))
 
@@ -94,6 +117,7 @@
                    (* (comp.b comp) (comp.q comp))))))
 
 (defun compute-series (series &key (lower 0) (upper 1000))
+  (declare (type integer lower upper))
   (computation-to-rational (binary-split series lower upper)))
 
 (defun exp-series (x)
@@ -109,16 +133,22 @@
                          (chud-b 545140134)
                          (chud-c 640320)
                          (chud-c^3 (/ (cube chud-c) 24)))
-                    (flet ((a (n) (+ chud-a (* chud-b n)))
-                           (p (n) (if (zerop n)
-                                      1
-                                      (* -1
-                                         (- (* 6 n) 5)
-                                         (- (* 2 n) 1)
-                                         (- (* 6 n) 1))))
-                           (q (n) (if (zerop n)
-                                      1
-                                      (* n n n chud-c^3))))
+                    (flet ((a (n)
+                             (declare (type integer n))
+                             (+ chud-a (* chud-b n)))
+                           (p (n)
+                             (declare (type integer n))
+                             (if (zerop n)
+                                 1
+                                 (* -1
+                                    (- (* 6 n) 5)
+                                    (- (* 2 n) 1)
+                                    (- (* 6 n) 1))))
+                           (q (n)
+                             (declare (type integer n))
+                             (if (zerop n)
+                                 1
+                                 (* n n n chud-c^3))))
                       (make-binsplit-series :a #'a
                                             :b (constantly 1)
                                             :p #'p
@@ -127,9 +157,16 @@
 (defconstant +chud-decimals-per-term+ 14.181647462d0)
 
 (defun compute-pi (prec)
+  (cl-stopwatch:stopwatch-start :sqrt)
   (let* ((chud-c 640320)
          (chud-c/12 (/ chud-c 12))
          (num-terms (floor (+ 2 (/ prec +chud-decimals-per-term+))))
          (sqrt-c (isqrt (* chud-c (expt 100 prec)))))
-    (values (floor  (* sqrt-c chud-c/12)
-                    (compute-series pi-series :upper num-terms)))))
+    (format t "sqrt: ~A~%" (cl-stopwatch:stopwatch-stop :sqrt))
+    (cl-stopwatch:stopwatch-start :pi)
+    (let ((pi-calculation (compute-series pi-series :upper num-terms)))
+      (format t "pi: ~A~%" (cl-stopwatch:stopwatch-stop :pi))
+      (cl-stopwatch:stopwatch-start :floor)
+      (prog1 (values (floor  (* sqrt-c chud-c/12)
+                             pi-calculation))
+        (format t "floor: ~A~%" (cl-stopwatch:stopwatch-stop :floor))))))
