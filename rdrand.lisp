7cc6ec5ff5762ac9da9185a7354fbe785c0bbefe
Robert Smith <quad@symbo1ics.com>
2012-09-11 08:10:56 -0500

    add (ignored) instruction format, change documentation a bit, use EMIT-MOD-REG-R/M-BYTE

--- rdrand.lisp
+++ rdrand.lisp
@@ -5,23 +5,43 @@
 
 (in-package "SB-VM")
 
+;;; Define the instruction format.
+
+#+#:ignore
+(sb!disassem:define-instruction-format (ext-two-byte-op-reg-no-width 32
+                                        :default-printer '(:name :tab reg))
+  ;; REX
+  (prefix  :field (byte 8 0)    ;; :value #b00001111
+           )
+  
+  ;; two byte operand
+  (op    :field (list (byte 5 11)))
+  
+  ;; Mod R/M register format
+  (reg   :field (byte 3 8) :type 'reg-b))
+
+
 ;;; The RDRAND instruction has the following layout:
 ;;; 
-;;;    32-bit: #x0FC7
+;;;    32-bit: #x0FC7 /6
 ;;; 
-;;;    64-bit: REX + #x0FC7
+;;;    64-bit: REX.W + #x0FC7 /6
 
 (define-instruction rdrand (segment dst)
-  ;;(:printer reg/mem ((op #x0FC7)))
+  (:printer ext-two-byte-op-reg-no-width ())             ; not quite right...
   (:emitter
    ;; Emit REX depending on register kind.
    (maybe-emit-rex-for-ea segment dst nil)
    
    ;; Emit the RDRAND instruction bits.
-   (emit-byte segment #x0FC7)
-   
+   (emit-byte segment #x0F)
+   (emit-byte segment #xC7)
+
    ;; Emit register code.
-   (emit-ea segment dst #b011)))
+   (emit-mod-reg-r/m-byte segment
+                          #b11
+                          #b110
+                          (reg-tn-encoding dst))))
 
 
 ;;; Tell the compiler about %RDRAND
@@ -34,13 +54,14 @@
   (:translate %rdrand)
   (:results (res :scs (any-reg)))
   (:result-types tagged-num)
-  (:policy :fast)
+  (:policy :fast-safe)
   (:generator 3
     RETRY-RDRAND
     (inst rdrand res)
     (inst jmp :nc RETRY-RDRAND)
-    (inst sar res sb-vm:n-fixnum-tag-bits)))
+    (inst shr res sb-vm:n-fixnum-tag-bits)))
 
-
-
+#+#:ignore
+(defun rdrand ()
+  (%rdrand))
 
