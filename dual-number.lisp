e4bf6f6fe5acaad39d9e8a9f8107d86553db2972
Robert Smith <quad@symbo1ics.com>
2012-09-21 15:32:27 -0500

    * Add CLOSER-MOP requirement. * Add pretty printer and predicate. * Add ENSURE-DUAL. * Fix DUAL-RECIPROCAL. * Define DUAL-FUNCTIONs with FUNCALL semantics. * Add DEFINE-DUAL-FUNCTION.

--- dual-number.lisp
+++ dual-number.lisp
@@ -1,14 +1,34 @@
 ;;;; dual-number.lisp
 ;;;; Copyright (c) 2012 Robert Smith
 
+;;; Requires MOP.
+#-closer-mop
+(ql:quickload "CLOSER-MOP")
+
+;;; Structure
+
 (defstruct (dual (:conc-name dual.)
-                 (:constructor dual (real &optional (epsilon 0))))
+                 (:constructor dual (real &optional (epsilon 0)))
+                 (:predicate dualp)
+                 (:print-function
+                  (lambda (obj str depth)
+                    (declare (ignore depth))
+                    (print-unreadable-object (obj str)
+                      (format str "~A ~:[+~;-~] ~Aε"
+                              (dual.real obj)
+                              (minusp (dual.epsilon obj))
+                              (dual.epsilon obj))))))
   real
   epsilon)
 
+(defun ensure-dual (x)
+  (etypecase x
+    (real (dual x))
+    (dual x)))
+
 (defmacro with-dual ((a b) dual-number &body body)
   (let ((g (gensym "G-")))
-    `(let* ((,g ,dual-number)
+    `(let* ((,g (ensure-dual ,dual-number))
             (,a (dual.real ,g))
             (,b (dual.epsilon ,g)))
        ,@body)))
@@ -47,8 +67,8 @@
 
 (defun dual-reciprocal (d)
   (with-dual (a b) d
-    (dual (/ p)
-          (- (/ q p p)))))
+    (dual (/ a)
+          (- (/ b a a)))))
 
 (defun dual-/ (d1 d2)
   (with-dual (a b) d1
@@ -57,4 +77,35 @@
             (/ (- (* b p) (* a q))
                p p)))))
 
+;;; A unary function f : R → R can be lifted to duals by defining f as
+;;; 
+;;;        f(a + bε) = f(a) + b·f′(a)ε.
+;;; 
+;;; We will call (f, f′) pairs "dual functions".
+
+(defclass dual-function ()
+  ((function :initarg :function
+             :accessor dual-function.function)
+   (derivative :initarg :derivative
+               :accessor dual-function.derivative))
+  (:metaclass c2mop:funcallable-standard-class))
+
+(defmethod initialize-instance :after ((df dual-function) &key)
+  (with-slots (function derivative) df
+    (c2mop:set-funcallable-instance-function
+     df
+     (lambda (dual-number)
+       (with-dual (a b) dual-number
+         (dual (funcall function a)
+               (* b (funcall derivative a))))))))
+
+(defmacro define-dual-function (name (argument) &key function derivative)
+  (check-type name     symbol)
+  (check-type argument symbol)
+  `(progn
+     (setf (symbol-function ',name)
+           (make-instance 'dual-function
+                          :function (lambda (,argument) ,function)
+                          :derivative (lambda (,argument) ,derivative)))
+     ',name))
 
