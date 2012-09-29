c8bfe7f7514794cd325d038fa5b0c8ca1882edba
Robert Smith <quad@symbo1ics.com>
2012-09-29 11:38:44 -0500

    Change DOPPLERIZE to output strings. Add SPACE-FREE-MODIFIED-DOPPLERIZE.

--- dopplerize.lisp
+++ dopplerize.lisp
@@ -4,12 +4,16 @@
 (defun dopplerize (string &key (growth-function 'identity))
   (labels ((spaces (n)
              (make-string (floor n) :initial-element #\Space)))
-    (loop :with length := (length string)
-          :for c :across string
-          :for i :from 1
-          :do (if (= i length) ; last char?
-                  (format t "~C~%" c)
-                  (format t "~C~A" c (spaces (funcall growth-function i)))))))
+    (with-output-to-string (s)
+      (loop :with length := (length string)
+            :for c :across string
+            :for i :from 1
+            :do (if (= i length) ; last char?
+                    (format s "~C" c)
+                    (format s "~C~A" c (spaces (funcall growth-function i))))))))
+
+(defun space-free-modified-dopplerize (string &key (growth-function 'identity))
+  (dopplerize (remove #\Space string) :growth-function growth-function))
 
 (defun triangle (n &optional (period 8))
   (let ((m (mod n period)))
