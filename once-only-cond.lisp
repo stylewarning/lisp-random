8ffc14a99246b015053f82fe1782ac1c80a9cd44
Robert Smith <quad@symbo1ics.com>
2012-10-31 09:20:54 -0500

    Add WITH-ONCE, a slight generalization.

--- once-only-cond.lisp
+++ once-only-cond.lisp
@@ -12,7 +12,7 @@
                                    ,@results))))))
          ,@body))))
 
-(defun test ()
+(defun test-cond-once ()
   (with-cond-once
     (dotimes (i 10)
       (let ((r (random 3)))
@@ -22,5 +22,28 @@
           ((= r 0) (princ "got 0"))
           ((= r 1) (princ "got 1"))
           ((= r 2) (princ "got 2")))
+        
+        (terpri)))))
+
+
+(defmacro with-once (&body body)
+  (let ((flags (gensym "FLAGS-")))
+    `(let ((,flags (make-hash-table)))
+       (macrolet ((once ()
+                    (let ((g (gensym "ONCE-")))
+                      `(unless (gethash ',g ,',flags)
+                         (setf (gethash ',g ,',flags) t)))))
+         ,@body))))
+
+(defun test-once ()
+  (with-once
+    (dotimes (i 10)
+      (let ((r (random 3)))
+        (format t "r=~D: " r)
+        
+        (cond-once
+          ((and (= r 0) (once)) (princ "got 0"))
+          ((and (= r 1) (once)) (princ "got 1"))
+          ((and (= r 2) (once)) (princ "got 2")))
         
         (terpri)))))
