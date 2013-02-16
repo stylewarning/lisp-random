016bcc293085fe7ec084da10e62c5a9d45ce393e
Robert Smith <quad@symbo1ics.com>
2013-02-15 23:56:06 -0800

    add +ZERO+, POLY-ZEROP, POLY-SCALE

--- polynomial.lisp
+++ polynomial.lisp
@@ -1,12 +1,14 @@
 ;;;; polynomial.lisp
 ;;;; Copyright (c) 2013 Robert Smith
 
+(defvar +zero+ #(0))
+
 (defun normalize-poly (p)
   "Normalize the polynomial P."
   (let ((last (position-if-not #'zerop p :from-end t))
         (length (length p)))
     (cond
-      ((null last) #(0))
+      ((null last) +zero+)
       ((= (1- length) last) p)
       (t (subseq p 0 (1+ last))))))
 
@@ -18,6 +20,9 @@
 (defun zero-poly (length)
   (make-array length :initial-element 0))
 
+(defun poly-zerop (p)
+  (every #'zerop p))
+
 (defun eval-poly (p x)
   "Evaluate the polynomial P at X."
   (reduce (lambda (next sum)
@@ -25,6 +30,10 @@
           p
           :initial-value 0
           :from-end t))
+
+(defun lc (p)
+  "Get the leading coefficient."
+  (aref p (1- (length p))))
 
 (defun poly-neg (p)
   "Negate a polynomial P."
@@ -48,6 +57,14 @@
 (defun poly-sub (p q)
   (poly-add p (poly-neg q)))
 
+(defun poly-scale (p factor)
+  "Scale the polynomial P by the factor FACTOR."
+  (if (zerop factor)
+      +zero+
+      (map 'vector (lambda (c)
+                     (* x factor))
+           p)))
+
 (defun poly-mul (p q)
   (let ((lp (length p))
         (lq (length q)))
@@ -63,3 +80,19 @@
                   :for i :below lp
                   :do (add! (aref p i) offset)
                   :finally (return (normalize-poly prod))))))))
+
+(defun poly-div (n d)
+  (if (poly-zerop d)
+      (error 'division-by-zero :operands (list n d)
+                               :operation '/)
+      (loop :with q := +zero+
+            :and  r := n
+            :and  temp := 0
+            :while (and (not (poly-zerop r))
+                        (>= (degree r)
+                            (degree d)))
+            ;; this is not correct.
+            :do (let ((next (/ (lc r) (lc d))))
+                  (psetf q (+ (aref q 0)) (+ q next)
+                         r (- r (* next d))))
+            :finally (return (values q r)))))
