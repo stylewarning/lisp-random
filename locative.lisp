1c3ee27ae74b6473eb09a9bf17e0eba0d64528fc
Robert Smith <quad@symbo1ics.com>
2013-02-18 17:11:10 -0800

    Example usage and some dox

--- locative.lisp
+++ locative.lisp
@@ -1,5 +1,24 @@
 ;;;; locative.lisp
 ;;;; Copyright (c) 2013 Robert Smith
+;;;;
+;;;; This is an example implementation of locatives by using closures
+;;;; and GET-SETF-EXPANSION.
+;;;;
+;;;; Locatives were like C pointers for the lisp machine, but were
+;;;; GC-safe. They were also efficiently represented, and specially
+;;;; handled.
+
+;;; Example usage:
+;;;
+;;; CL-USER> (let* ((x (make-array 5 :initial-element 0))
+;;;                 (l (locative-for (aref x 2))))
+;;;            (setf (dereference l) 5)
+;;;            (list l
+;;;                  x
+;;;                  (dereference l)))
+;;;
+;;; (#<LOCATIVE {100612B943}> #(0 0 5 0 0) 5)
+
 
 (defstruct (locative (:predicate locativep)
                      (:constructor %make-locative)
@@ -26,6 +45,7 @@
   (funcall (locative-reader locative)))
 
 ;;; This DEFSETF is actually incorrect. It is possible that the writer
-;;; function can take multiple values.
+;;; function can take multiple values. To use the full functionality,
+;;; one must simply use LOCATIVE-WRITER.
 (defsetf dereference (locative) (new-val)
   `(funcall (locative-writer ,locative) ,new-val))
