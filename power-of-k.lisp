d682c46d997eef24a6060c97a59dbc4f81bb1b0b
Robert Smith <quad@symbo1ics.com>
2013-03-20 12:38:32 -0700

    Add fusion code.

--- power-of-k.lisp
+++ power-of-k.lisp
@@ -1,6 +1,6 @@
 (defun binary-of (n)
   "Compute the binary digits of N compactly."
-  (let* ((binary (make-array (integer-length n) :element-type 'bit
+  (let ((binary (make-array (integer-length n) :element-type 'bit
                                                 :initial-element 0)))
     (labels ((doit (n i)
                (if (zerop n)
@@ -28,3 +28,18 @@ COEFFS at BASE."
 (defun smallest-sum-of-powers (base k)
   "Find the Kth smallest sum of unique powers of BASE."
   (horner base (binary-of k)))
+
+
+(defun fusion (base n)
+  "Fusion of the above three functions."
+  (labels ((doit (n power sum)
+             (if (zerop n)
+                 sum
+                 (multiple-value-bind (quo rem)
+                     (floor n 2)
+                   (if (= rem 1)
+                       (doit quo (* base power) (+ power sum))
+                       (doit quo (* base power) sum))))))
+    (if (zerop n)
+        0
+        (doit n 1 0))))
