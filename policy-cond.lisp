fa85a09b9aa9df084405cb4501bcad310d8e8965
Robert Smith <quad@symbo1ics.com>
2013-04-23 21:27:17 -0700

    start of policy-cond

---
+++ policy-cond.lisp
@@ -0,0 +1,34 @@
+;;;; policy-case.lisp
+;;;; Copyright (c) 2013 Robert Smith
+
+;;;; DOES NOT WORK
+
+(defun same-name (s1 s2)
+  (string= (symbol-name s1)
+           (symbol-name s2)))
+
+(defun policy (env)
+  (sb-cltl2:declaration-information 'optimize env))
+
+(defmacro policy-if (policy-expression then else &environment env)
+  (if (funcall (compile nil `(lambda ()
+                               (let ((policy (policy env)))
+                                 `(let ,policy
+                                    (declare (ignorable ,@(mapcar #'car policy)))
+                                    ,policy-expression)))))
+      then
+      else))
+
+(declaim (optimize (speed 3) (safety 0)))
+
+(defun foo ()
+  (policy-if (> speed safety)
+             (+ 1 1)
+             (* 4 4)))
+
+(declaim (optimize (speed 0) (safety 3)))
+
+(defun bar ()
+  (policy-if (> speed safety)
+             (+ 1 1)
+             (* 4 4)))
