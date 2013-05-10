683958227cdead3fc2614e008d0b08d33df2a313
Robert Smith <quad@symbo1ics.com>
2013-05-10 00:05:01 -0700

    add info about with-specialized-types

--- with-specialized-types.lisp
+++ with-specialized-types.lisp
@@ -1,6 +1,10 @@
 ;;;; specialize-type.lisp
 ;;;; Copyright (c) 2013 Robert Smith
 
+;;; We trade space for speed for space here. Compilers like SBCL can
+;;; specialize code paths in each branch of the ETYPECASE since it
+;;; knows the type of VAL. Maybe useful for numerical code.
+
 (defmacro with-specialized-types (val types &body body)
   `(etypecase ,val
      ,@(loop :for ty :in types
