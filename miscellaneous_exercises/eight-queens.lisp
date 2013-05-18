33b39de77f81d2a09a4f0c5aaf6592ec5a0da527
Robert Smith <quad@symbo1ics.com>
2013-05-18 06:39:08 -0700

    cache the queens, add documentation

--- miscellaneous_exercises/eight-queens.lisp
+++ miscellaneous_exercises/eight-queens.lisp
@@ -3,9 +3,20 @@
 
 ;;;; The classical eight queens problem.
 
-(defstruct queen
-  row
-  col)
+(defstruct queen row col)
+
+(defun queens (n)
+  "Create a function which generates new queens for a board of size
+N. The resulting function will take the row and column of a queen ROW
+and COL, and return a queen at that position."
+  (let ((cache (make-hash-table)))
+    (lambda (row col)
+      (let* ((coord (+ col (* row n)))
+             (lookup (gethash coord cache)))
+        (if lookup
+            lookup
+            (setf (gethash coord cache)
+                  (make-queen :row row :col col)))))))
 
 (defun attacksp (p q)
   "Does the queen Q attack queen P?"
@@ -21,21 +32,46 @@
         (diag= p q))))
 
 (defun safep (new-queen queens)
+  "Is the queen NEW-QUEEN safe with the set of queens QUEENS?"
   (notany (lambda (queen)
             (attacksp queen new-queen))
           queens))
 
 (defun solve (n)
-  (labels ((rec (x y pos sols)
-             (cond
-               ((> x n) (cons (reverse pos) sols))
-               ((> y n) sols)
-               (t (let ((q (make-queen :row x :col y)))
-                    (if (safep q pos)
-                        (rec x (1+ y) pos (rec (1+ x) 1 (cons q pos) sols))
-                        (rec x (1+ y) pos sols)))))))
-    (rec 1 1 nil nil)))
+  "Compute the solutions to the N-queens problem."
+  (let ((queen (queens n)))
+    (labels ((rec (row col pos sols)
+               (cond
+                 ;; We've reached the last row. Accumulate the
+                 ;; solutions.
+                 ((= row n) (cons (reverse pos) sols))
+
+                 ;; We've reached the last column, return the
+                 ;; solutions found so far.
+                 ((= col n) sols)
+                 
+                 ;; Check the next positions. If the queen is safe,
+                 ;; add it, and compute the possible arrangements of
+                 ;; queens in the next row, and continue by placing
+                 ;; the queen in the next column.
+                 (t (let ((q (funcall queen row col)))
+                      (if (safep q pos)
+                          (rec row (1+ col) pos (rec (1+ row) 0 (cons q pos) sols))
+                          (rec row (1+ col) pos sols)))))))
+      (rec 0 0 nil nil))))
 
 (defun bench (&optional (max 11))
   (dotimes (i max)
-    (format t "~A queens problem: ~A~%" i (length (solve (1+ i))))))
+    (format t "~A queens problem: ~A~%" (1+ i) (length (solve (1+ i))))))
+
+;;; START
+;; User time             =        1.434
+;; System time             =        0.004
+;; Elapsed time             =        1.454
+;; Allocation   = 76066680 bytes
+
+;;; QUEEN CACHING
+;; User time             =        1.381
+;; System time             =        0.002
+;; Elapsed time             =        1.387
+;; Allocation   = 4185256 bytes
