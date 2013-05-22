82c56c2b490878bae1cd08e7576656781b5e699d
Robert Smith <quad@symbo1ics.com>
2013-05-22 12:08:53 -0700

    remove REVERSE-WORD!

--- miscellaneous_exercises/reverse-words.lisp
+++ miscellaneous_exercises/reverse-words.lisp
@@ -4,25 +4,25 @@
 ;;;; Given a string such as "abc def", reverse the words, delimited by
 ;;;; spaces, in the string in place.
 
-(defun reverse-word! (string start)
-  "Reverse the word in the string STRING starting at the index START
-in place. Return the index after the reversed word."
-  (let* ((end (or (position #\Space string :start start
-                                           :test #'char=)
-                  (length string)))
-         (substring-length (- end start)))
+(defun reverse-substring! (string start end)
+  "Reverse the substring of characters in the string STRING from the
+index START to before the index END."
+  (let ((substring-length (- end start)))
     (loop :for i :from start :below (+ start (floor substring-length 2))
           :do (rotatef (aref string i)
-                       (aref string (- end (- i start) 1)))
-          :finally (return (1+ end)))))
+                       (aref string (- end (- i start) 1))))))
 
 (defun reverse-words (string)
   "Reverse the words in the string STRING in place."
   (let ((len (length string)))
-    (labels ((next-word (word-start)
+    (labels ((reverse-next-word (word-start)
                (if (>= word-start len)
                    string
-                   (next-word (reverse-word string word-start)))))
+                   (let ((word-end (or (position #\Space string :start word-start
+                                                                :test #'char=)
+                                       len)))
+                     (reverse-substring! string word-start word-end)
+                     (reverse-next-word (1+ word-end))))))
       (next-word 0))))
 
 ;; Note that 0 bytes were allocated during the REVERSE-WORDS
