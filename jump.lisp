a383c3475141ecfb74f4d87096b6b6ba5ef171b9
Robert Smith <quad@symbo1ics.com>
2013-06-25 09:52:39 -0700

    Enhance jump table to allow gaps and default values.

--- jump.lisp
+++ jump.lisp
@@ -16,22 +16,26 @@
           :for x :in sorted
           :always (= i x))))
 
-(defmacro jump (n &body cases)
-  "Efficiently execute the body of a case in CASES and return
-depending on the value of N.
+(defmacro jump ((n &optional default) &body cases)
+  "Efficiently execute the body of a case in CASES chosen by the
+non-negative integer N. If there is no case matching N and N is less
+than or equal to the maximum possible case outlined in CASES, then
+return DEFAULT, which is only evaluated if returned.
 
-N should be a non-negative integer below the total number of cases. The syntax of a case is
+Syntax:
 
-    (NUM FORM*)
-
-where NUM is an integer having the same values as N.
+    jump-form := (JUMP (<expr> <expr>?) <case>+)
+    case-form := (<value-form> <expr>*)
+    value-form := <non-negative integer>
+                | (<non-negative integer>+)
 
 Example:
 
-    (jump (random 3)
-      (0 (print 0) 'zero)
-      (1 (print 1) 'one)
-      (2 (print 2) 'two))
+    (jump (5 'nope)
+      (0 'zero)
+      ((1 3 5) 'odd)
+      ((2 4 6) 'even)
+      (7 'prime))
 "
   (assert (not (null cases))
           (cases)
@@ -39,44 +43,76 @@ Example:
   (assert (every #'(lambda (jump-case)
                      (and (listp jump-case)
                           (not (null jump-case))
-                          (integerp (car jump-case))
-                          (not (minusp (car jump-case)))))
+                          (or (and (integerp (car jump-case))
+                                   (not (minusp (car jump-case))))
+                              (and (every #'integerp (car jump-case))
+                                   (notany #'minusp (car jump-case))))))
                  cases)
           (cases)
-          "The cases must be lists whose first element is a positive integer.")
-  (assert (contains-below-n (mapcar #'car cases))
-          (cases)
-          "The cases must contain all integers between 0 and ~A inclusive."
-          (1- (length cases)))
-  `(funcall
-    (the (function () t)
-         (svref (constant-load-time-value
-                 (make-array ,(length cases)
-                             :element-type '(function () t)
-                             :initial-contents
-                             ,(cons 'list
-                                    (loop :for i :below (length cases)
-                                          :collect `(lambda ()
-                                                      ,@(cdr (assoc i cases)))))))
-                ,n))))
+          "The cases must be lists whose first element is a positive integer or list of positive integers.")
+  (let ((table    (make-hash-table))
+        (bindings nil)
+        (maxval   0))
+    (labels ((ensure-list (x)
+               (if (listp x)
+                   x
+                   (list x)))
+             
+             (populate-table ()
+               (dolist (jump-case cases)
+                 (let ((indexes (car jump-case))
+                       (binding-var (gensym))
+                       (lambda-form `(lambda () ,@(cdr jump-case))))
+                   (push (list binding-var lambda-form) bindings)
+                   (dolist (index (ensure-list indexes))
+                     (when (> index maxval)
+                       (setf maxval index))
+                     (setf (gethash index table) binding-var)))))
+             
+             (table-to-initial-contents ()
+               (let ((default-var (gensym "DEFAULT-VAR-")))
+                 `(let* ((,default-var (lambda () ,default))
+                        ,@bindings)
+                    (declare (ignorable ,default-var))
+                    ,(cons 'list
+                           (loop :for i :to maxval
+                                 :collect (if (gethash i table)
+                                              (gethash i table)
+                                              default-var)))))))
+      
+      (populate-table)
+      
+      `(funcall
+        (the (function () t)
+             (svref (constant-load-time-value
+                     (make-array ,(1+ maxval)
+                                 :element-type '(function () t)
+                                 :initial-contents
+                                 ,(table-to-initial-contents)))
+                    ,n))))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Tests ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defparameter *trials* 10000000)
 
-(defmacro generate-test-form (name num-cases)
-  `(,name (random ,num-cases)
-          ,@(loop :for i :below num-cases
-                  :collect (list i i))))
+(defmacro generate-jump-form (num-cases)
+  `(jump ((random ,num-cases))
+     ,@(loop :for i :below num-cases
+             :collect (list i i))))
+
+(defmacro generate-case-form (num-cases)
+  `(case (random ,num-cases)
+     ,@(loop :for i :below num-cases
+             :collect (list i i))))
 
 (defun test-jump ()
   (loop :repeat *trials*
-        :do (generate-test-form jump 1000)))
+        :do (generate-jump-form 1000)))
 
 (defun test-case ()
   (loop :repeat *trials*
-        :do (generate-test-form case 1000)))
+        :do (generate-case-form 1000)))
 
 ;; CL-USER> (progn
 ;;            (time (test-jump))
