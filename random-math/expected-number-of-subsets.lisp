628d5b364d8d264bfb9b8328728b635b887ebf5c
Robert Smith <quad@symbo1ics.com>
2013-07-28 14:16:57 -0700

    differences

--- random-math/expected-number-of-subsets.lisp
+++ random-math/expected-number-of-subsets.lisp
@@ -17,6 +17,13 @@
                  :sum (run-simulation n) :into s
                  :finally (return (float (/ s trials))))))
     (loop :for i :from 2 :to max
-          :do (format t "~D: ~D~%" i (run-sim i)))))
+          :collect (run-sim i))))
+
+(defun differences (a b)
+  (mapcar #'(lambda (x y) (- x y)) a b))
+
+(defun sim-differences (n)
+  (let ((sims (run-sims n)))
+    (differences (cdr sims) sims)))
 
 ;;; Algebraist says E(n) is f(n)=1+sum_i=0^n (n choose i) 2^{-n} f(i)
