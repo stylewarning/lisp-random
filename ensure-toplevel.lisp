506491af391e645b3cc7acfa2915fbdf0715fdfa
Robert Smith <quad@symbo1ics.com>
2015-03-24 01:03:02 -0700

    Add ENSURE-TOPLEVEL.

---
+++ ensure-toplevel.lisp
@@ -0,0 +1,31 @@
+;;;; ensure-toplevel.lisp
+;;;;
+;;;; Copyright (c) 2013 Robert Smith
+
+(defmacro ensure-toplevel (&body body)
+  "Ensures that BODY is executed at the top level. Otherwise, an error occurs."
+  (let ((sym (gensym "TOPLEVEL-CHECKER-")))
+    `(progn
+       (eval-when (:compile-toplevel :load-toplevel)
+         (setf (symbol-value ',sym) t))
+       (eval-when (:execute)
+         (unless (boundp ',sym)
+           (error "A given form was not found at the top level when it was asserted to be. Found:~%~4T~S" '(progn ,@body))))
+       ,@body)))
+
+#+#:IGNORE
+(progn
+  ;; OK
+  (ensure-toplevel
+    (defparameter *foo* 1))
+
+  ;; OK
+  (ensure-toplevel
+    (defun f (x)
+      (* x x)))
+
+  ;; ERROR
+  (let ((counter 1))
+    (ensure-toplevel
+      (defun g ()
+        (incf counter)))))
