7212bbf8e388d1d8cb2bd9c5acb98ac2f8bfc4fa
Robert Smith <quad@symbo1ics.com>
2015-03-24 22:08:03 -0700

    Error at compile time. Rename to ASSERT-TOPLEVEL.

--- ensure-toplevel.lisp
+++ ensure-toplevel.lisp
@@ -2,41 +2,44 @@
 ;;;;
 ;;;; Copyright (c) 2015 Robert Smith
 
-;;; Bug: This should error at compile time, but currently only errors
-;;;      at load time.
-(defmacro ensure-toplevel (&body body)
+;;; Thanks to Bike from IRC. He thought of the critical ideas to make
+;;; this work correctly.
+
+(defmacro assert-toplevel (&body body)
   "Checks that BODY will be executed at the top level. Otherwise, an error occurs."
   (let ((at-toplevel-p (gensym "AT-TOPLEVEL-P-")))
     `(progn
        (eval-when (:compile-toplevel :load-toplevel)
          (setf (symbol-value ',at-toplevel-p) t))
        (eval-when (:execute)
-         (load-time-value
-          (unless (boundp ',at-toplevel-p)
-            (error "A given form was not found at the top level when it ~
-                    was asserted to be. Found:~%~4T~S"
-                   '(progn ,@body)))))
+         (macrolet ((compile-time-check ()
+                      (unless (boundp ',at-toplevel-p)
+                        (cerror "Go ahead anyway."
+                                "A given form was not found at the top level ~
+                                when it was asserted to be. Found:~%~4T~S"
+                               '(progn ,@body)))))
+           (compile-time-check)))
        ,@body)))
 
 #+#:IGNORE
 (progn
   ;; OK
-  (ensure-toplevel
+  (assert-toplevel
     (defparameter *foo* 1))
 
   ;; OK
-  (ensure-toplevel
+  (assert-toplevel
     (defun f (x)
       (* x x)))
 
   ;; ERROR
   (let ((counter 1))
-    (ensure-toplevel
+    (assert-toplevel
       (defun g ()
         (incf counter))))
   
   ;; ERROR
   (defun f (x)
-    (ensure-toplevel
+    (assert-toplevel
       (defun g (x)
         (* x x)))))
