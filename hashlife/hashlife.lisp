ea5f4e093eb14f16672b00c3914927688b6720d1
Robert Smith <quad@symbo1ics.com>
2015-10-08 17:31:25 -0700

    Make simple pretty printer #'SHOW

--- hashlife/hashlife.lisp
+++ hashlife/hashlife.lisp
@@ -181,6 +181,32 @@ The coordinate system is defined to start at (-2^(LEVEL - 1), 2^(LEVEL - 1)) in 
                   (macrocell-cell (macrocell-ne mc) (- x offset) (+ y offset))
                   (macrocell-cell (macrocell-se mc) (- x offset) (- y offset))))))))
 
+(defun show (mc &optional (stream *standard-output*))
+  "Pretty print a macrocell MC to the stream STREAM, which is *STANDARD-OUTPUT* by default."
+  (let* ((width (macrocell-width mc))
+         (width/2 (/ width 2)))
+    ;; Draw top border.
+    (write-char #\+ stream)
+    (loop :repeat width :do (write-char #\- stream))
+    (write-char #\+ stream)
+    (terpri stream)
+    
+    ;; Draw pattern.
+    (loop :for y :from (- width/2) :below width/2 :do
+      (write-char #\| stream)
+      (loop :for x :from (- width/2) :below width/2 :do
+        (if (= 1 (macrocell-cell mc x y))
+            (write-char #\* stream)
+            (write-char #\Space stream)))
+      (write-char #\| stream)
+      (terpri stream))
+    
+    ;; Draw top border.
+    (write-char #\+ stream)
+    (loop :repeat width :do (write-char #\- stream))
+    (write-char #\+ stream)
+    (terpri stream)))
+
 (defconstant +neighborhood-mask+ #b000011101010111
   "The bits of the neighborhood of a cell. The cell would be located at the 5th bit (zero indexed). See the documentation for #'NEXT-GENERATION-CELL for more information.")
 
