999aca88d4c2934e40bbafb5fa48525a511ab7bf
Robert Smith <quad@symbo1ics.com>
2015-10-09 19:29:49 -0700

    add some exports

--- hashlife/package.lisp
+++ hashlife/package.lisp
@@ -3,5 +3,14 @@
 ;;;; Copyright (c) 2015 Robert Smith
 
 (defpackage #:hashlife
-  (:use #:cl))
+  (:use #:cl)
+  (:export
+   #:timestep                           ; FUNCTION
+   #:hyperstep                          ; FUNCTION
+   #:empty-macrocell                    ; FUNCTION
+   #:pad-macrocell                      ; FUNCTION
+   #:macrocell-cell                     ; FUNCTION
+   #:macrocell-set-cell                 ; FUNCTION
+   )
+  )
 
