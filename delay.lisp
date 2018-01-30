197436ea8328df9ce036d7c22f76ec521981a5d9
Robert Smith <robert@stylewarning.com>
2018-01-30 12:31:36 -0800

    add some useful primitives

--- delay.lisp
+++ delay.lisp
@@ -1,5 +1,31 @@
 ;;;; delay.lisp
-;;;; Copyright (c) 2012-2013 Robert Smith
+;;;; Copyright (c) 2012-2018 Robert Smith
+
+(defpackage #:lazy
+  (:use #:cl)
+  (:shadow ;; #:cons
+           #:car
+           #:cdr
+           #:append)
+  (:export #:promise
+           #:force
+           #:delay
+           #:call-with-delayed-arguments
+           #:lazycall
+           #:deflazy
+           #:enable-lazy-syntax
+           ;; Shadowed symbols
+           ;; #:cons
+           #:car
+           #:cdr
+           #:append
+           #:integers
+           #:repeat
+           #:take
+           #:force-tree
+           ))
+
+(in-package #:lazy)
 
 ;;;; A small library for lazy function evaluation, including optional
 ;;;; special syntax.
@@ -16,12 +42,14 @@
 
 (defun force (promise)
   "Force a promise PROMISE. Return the value of the promise and
-memoize it."
-  (declare (type promise promise))
-  (if (promise.forcedp promise)
-      (promise.value promise)
-      (setf (promise.forcedp promise) t
-            (promise.value promise) (funcall (promise.value promise)))))
+memoize it. If something that's not a PROMISE is given, return it as-is."
+  (typecase promise
+    (promise
+     (if (promise.forcedp promise)
+         (promise.value promise)
+         (setf (promise.forcedp promise) t
+               (promise.value promise) (force (funcall (promise.value promise))))))
+    (t promise)))
 
 (defmacro delay (&body computation)
   "Delay the computation of COMPUTATION, creating a promise."
@@ -73,6 +101,39 @@ memoize it."
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; EXAMPLES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(enable-lazy-syntax)
+
+(deflazy car (c)
+  (assert (consp c))
+  (force (cl:car c)))
+
+(deflazy cdr (c)
+  (assert (consp c))
+  (force (cl:cdr c)))
+
+(defun append (x y)
+  (delay
+    (if (null (force x))
+        y
+        [cons (car x) (append (cdr x) y)])))
+
+(defun integers (&optional (start 0))
+  (delay (cons start (integers (1+ start)))))
+
+(defun repeat (x)
+  (delay (cons x (repeat x))))
+
+(deflazy take (n x)
+  (cond
+    ((zerop n) nil)
+    ((null x)  nil)
+    (t (delay [cons (car x) (take (1- n) (cdr x)) ]))))
+
+(deflazy force-tree (x)
+  (cond
+    ((atom x) x)
+    ((consp x) (cons (force-tree (car x)) (force-tree (cdr x))))))
+
 (deflazy if* (pred then else)
   (if pred then else))
 
@@ -80,9 +141,9 @@ memoize it."
   (if (functionp #'if*)
       (format t "Yes ~S is a function!" 'if*)
       (format t "ERROR! ~S is not a function!" 'if*))
-  
+
   (terpri)
-  
+
   (lazycall 'if*
             t
             (format t "~S worked!~%" 'if*)
